a2f119e517406ec987833e49fc28e350c3a2978c
RustamAxm <rustamaxm@gmail.com>
2022-04-07 00:06:20 +0300

    ProcessQueriesJoined and split tests

--- search-server/process_queries.cpp
+++ search-server/process_queries.cpp
@@ -8,16 +8,27 @@
 std::vector<std::vector<Document>> ProcessQueries(
         const SearchServer& search_server,
         const std::vector<std::string>& queries){
-    std::vector<std::vector<Document>> documents_lists;
-    documents_lists.reserve(queries.size());
+    std::vector<std::vector<Document>> documents_lists(queries.size());
 
-    std::transform( std::execution::par,
+    std::transform(std::execution::par,
                     queries.begin(), queries.end(),
                    documents_lists.begin(),
                    [&search_server](const std::string& query){
                     return search_server.FindTopDocuments(query); });
-//    for (const std::string& query : queries) {
-//        documents_lists.push_back(search_server.FindTopDocuments(query));
-//    }
+
     return documents_lists;
+}
+
+std::vector<Document> ProcessQueriesJoined(
+        const SearchServer& search_server,
+        const std::vector<std::string>& queries){
+    std::vector<Document> joined;
+
+    for (const auto& documents : ProcessQueries(search_server, queries)){
+        std::transform(
+                       documents.begin(), documents.end(),
+                       std::back_inserter(joined),
+                       [](const auto& x) {return x;});
+    }
+    return joined;
 }
